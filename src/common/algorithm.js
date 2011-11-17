d0088192e68a11d4f3065ab1607f10944109a082
JSilver99 <jasilver1@learn.senecac.on.ca>
2011-11-17 18:07:30 -0500

    Refactored ClassicalNoise (Perlin) to be more in line with current JS style. Added an init function so that it can be reseeded at will.

--- src/common/algorithm.js
+++ src/common/algorithm.js
@@ -11,34 +11,40 @@ var algorithm = function(options) {
   * It is assumed to have a random() method.
   */
   var ClassicalNoise = function(r) { // Classic Perlin noise in 3D, for comparison
-  if (r == undefined) r = Math;
+    var that = this;
     this.grad3 = [[1,1,0],[-1,1,0],[1,-1,0],[-1,-1,0],
-                                   [1,0,1],[-1,0,1],[1,0,-1],[-1,0,-1],
-                                   [0,1,1],[0,-1,1],[0,1,-1],[0,-1,-1]];
-    this.p = [];
-    for (var i=0; i<256; i++) {
-  this.p[i] = Math.floor(r.random()*256);
-    }
-    // To remove the need for index wrapping, double the permutation table length
-    this.perm = [];
-    for(var i=0; i<512; i++) {
-  this.perm[i]=this.p[i & 255];
-    }
-  };
+                  [1,0,1],[-1,0,1],[1,0,-1],[-1,0,-1],
+                  [0,1,1],[0,-1,1],[0,1,-1],[0,-1,-1]];
+  
+    this.init = function(r) {
+      if (r == undefined) r = Math;
+      that.p = [];
+      for (var i=0; i<256; i++) {
+        that.p[i] = Math.floor(r.random()*256);
+      }
+      // To remove the need for index wrapping, double the permutation table length
+      that.perm = [];
+      for(var i=0; i<512; i++) {
+        that.perm[i]=that.p[i & 255];
+      }
+    };
+    
+    that.init(r);
+  };  
   
   ClassicalNoise.prototype.dot = function(g, x, y, z) {
-      return g[0]*x + g[1]*y + g[2]*z;
+    return g[0]*x + g[1]*y + g[2]*z;
   };
   
   ClassicalNoise.prototype.mix = function(a, b, t) {
-      return (1.0-t)*a + t*b;
+    return (1.0-t)*a + t*b;
   };
   
   ClassicalNoise.prototype.fade = function(t) {
       return t*t*t*(t*(t*6.0-15.0)+10.0);
   };
-  
-    // Classic Perlin noise, 3D version
+
+  // Classic Perlin noise, 3D version
   ClassicalNoise.prototype.noise = function(x, y, z) {
     // Find unit grid cell containing point
     var X = Math.floor(x);
@@ -56,51 +62,41 @@ var algorithm = function(options) {
     Z = Z & 255;
     
     // Calculate a set of eight hashed gradient indices
-    var gi000 = this.perm[X+this.perm[Y+this.perm[Z]]] % 12;
-    var gi001 = this.perm[X+this.perm[Y+this.perm[Z+1]]] % 12;
-    var gi010 = this.perm[X+this.perm[Y+1+this.perm[Z]]] % 12;
-    var gi011 = this.perm[X+this.perm[Y+1+this.perm[Z+1]]] % 12;
-    var gi100 = this.perm[X+1+this.perm[Y+this.perm[Z]]] % 12;
-    var gi101 = this.perm[X+1+this.perm[Y+this.perm[Z+1]]] % 12;
-    var gi110 = this.perm[X+1+this.perm[Y+1+this.perm[Z]]] % 12;
-    var gi111 = this.perm[X+1+this.perm[Y+1+this.perm[Z+1]]] % 12;
+    var gi000 = that.perm[X+that.perm[Y+that.perm[Z]]] % 12;
+    var gi001 = that.perm[X+that.perm[Y+that.perm[Z+1]]] % 12;
+    var gi010 = that.perm[X+that.perm[Y+1+that.perm[Z]]] % 12;
+    var gi011 = that.perm[X+that.perm[Y+1+that.perm[Z+1]]] % 12;
+    var gi100 = that.perm[X+1+that.perm[Y+that.perm[Z]]] % 12;
+    var gi101 = that.perm[X+1+that.perm[Y+that.perm[Z+1]]] % 12;
+    var gi110 = that.perm[X+1+that.perm[Y+1+that.perm[Z]]] % 12;
+    var gi111 = that.perm[X+1+that.perm[Y+1+that.perm[Z+1]]] % 12;
     
-    // The gradients of each corner are now:
-    // g000 = grad3[gi000];
-    // g001 = grad3[gi001];
-    // g010 = grad3[gi010];
-    // g011 = grad3[gi011];
-    // g100 = grad3[gi100];
-    // g101 = grad3[gi101];
-    // g110 = grad3[gi110];
-    // g111 = grad3[gi111];
     // Calculate noise contributions from each of the eight corners
-    var n000= this.dot(this.grad3[gi000], x, y, z);
-    var n100= this.dot(this.grad3[gi100], x-1, y, z);
-    var n010= this.dot(this.grad3[gi010], x, y-1, z);
-    var n110= this.dot(this.grad3[gi110], x-1, y-1, z);
-    var n001= this.dot(this.grad3[gi001], x, y, z-1);
-    var n101= this.dot(this.grad3[gi101], x-1, y, z-1);
-    var n011= this.dot(this.grad3[gi011], x, y-1, z-1);
-    var n111= this.dot(this.grad3[gi111], x-1, y-1, z-1);
+    var n000= that.dot(that.grad3[gi000], x, y, z);
+    var n100= that.dot(that.grad3[gi100], x-1, y, z);
+    var n010= that.dot(that.grad3[gi010], x, y-1, z);
+    var n110= that.dot(that.grad3[gi110], x-1, y-1, z);
+    var n001= that.dot(that.grad3[gi001], x, y, z-1);
+    var n101= that.dot(that.grad3[gi101], x-1, y, z-1);
+    var n011= that.dot(that.grad3[gi011], x, y-1, z-1);
+    var n111= that.dot(that.grad3[gi111], x-1, y-1, z-1);
     // Compute the fade curve value for each of x, y, z
-    var u = this.fade(x);
-    var v = this.fade(y);
-    var w = this.fade(z);
+    var u = that.fade(x);
+    var v = that.fade(y);
+    var w = that.fade(z);
      // Interpolate along x the contributions from each of the corners
-    var nx00 = this.mix(n000, n100, u);
-    var nx01 = this.mix(n001, n101, u);
-    var nx10 = this.mix(n010, n110, u);
-    var nx11 = this.mix(n011, n111, u);
+    var nx00 = that.mix(n000, n100, u);
+    var nx01 = that.mix(n001, n101, u);
+    var nx10 = that.mix(n010, n110, u);
+    var nx11 = that.mix(n011, n111, u);
     // Interpolate the four results along y
-    var nxy0 = this.mix(nx00, nx10, v);
-    var nxy1 = this.mix(nx01, nx11, v);
+    var nxy0 = that.mix(nx00, nx10, v);
+    var nxy1 = that.mix(nx01, nx11, v);
     // Interpolate the two last results along z
-    var nxyz = this.mix(nxy0, nxy1, w);
+    var nxyz = that.mix(nxy0, nxy1, w);
   
     return nxyz;
   };
-  
   
   // Ported from Stefan Gustavson's java implementation
   // http://staffwww.itn.liu.se/~stegu/simplexnoise/simplexnoise.pdf
@@ -296,8 +292,8 @@ var algorithm = function(options) {
           return seed; 
         }
       };
-      _perlinNoise.Init(_randomObject);
-      _simplexNoise.Init(_randomObject);
+      _perlinNoise.init(_randomObject);
+      _simplexNoise.init(_randomObject);
     } else {
       throw "You must seed with a number.";
     }
